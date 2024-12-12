ef3e95ee49a0c5bcebc197c21aadc05023909a26
salekelfahim <salekelfahim@gmail.com>
2024-12-12 09:35:55 +0100

    Modifying in the secutity

--- src/main/java/org/youcode/maska_hunters_league/config/SecurityConfiguration.java
+++ src/main/java/org/youcode/maska_hunters_league/config/SecurityConfiguration.java
@@ -3,17 +3,26 @@ package org.youcode.maska_hunters_league.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.youcode.maska_hunters_league.security.JwtAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.youcode.maska_hunters_league.domain.enums.Permission.*;
+import static org.youcode.maska_hunters_league.domain.enums.Role.*;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -23,17 +32,20 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(
+                        authorizeHttpRequest -> {
+                            authorizeHttpRequest.requestMatchers("/api/v1/auth/**")
+                                    .permitAll();
+                            authorizeHttpRequest.requestMatchers("/api/v1/hunt").hasAnyRole(ADMIN.name(), JURY.name());
+                            authorizeHttpRequest.requestMatchers(POST,"/api/v1/participation/**").hasAuthority(CAN_PARTICIPATE.name());
+                            authorizeHttpRequest.requestMatchers(GET,"/api/v1/participation/**").hasAuthority(CAN_VIEW_RANKINGS.name());
+                            authorizeHttpRequest.requestMatchers(GET,"/api/v1/participation/**").hasAuthority(CAN_VIEW_RANKINGS.name());
+                            authorizeHttpRequest.requestMatchers("/api/v1/users/**").hasRole(ADMIN.name());
+                            authorizeHttpRequest.anyRequest().authenticated();
+                        }
+                ).sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
---
+++ src/main/java/org/youcode/maska_hunters_league/domain/enums/Permission.java
@@ -0,0 +1,12 @@
+package org.youcode.maska_hunters_league.domain.enums;
+
+public enum Permission {
+    CAN_PARTICIPATE,
+    CAN_VIEW_RANKINGS,
+    CAN_VIEW_COMPETITIONS,
+    CAN_SCORE,
+    CAN_MANAGE_COMPETITIONS,
+    CAN_MANAGE_USERS,
+    CAN_MANAGE_SPECIES,
+    CAN_MANAGE_SETTINGS;
+}
