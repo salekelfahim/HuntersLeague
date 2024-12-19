1b3c578a0e94522875621efdcf53531d1355ee64
salekelfahim <salekelfahim@gmail.com>
2024-12-19 12:06:54 +0100

    n

--- src/test/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplicationTests.java
+++ src/test/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplicationTests.java
@@ -3,11 +3,12 @@ package org.youcode.maska_hunters_league;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.name=application-test")
 class MaskaHuntersLeagueApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test ensures that the Spring application context loads successfully.
     }
 
 }
